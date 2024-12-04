13e0557ef93df88d013c76e2db6c2b2696515944
evanbacon <bacon@expo.io>
2024-12-04 15:42:57 -0600

    Add ensureFrameworks function

--- src/api/PBXNativeTarget.ts
+++ src/api/PBXNativeTarget.ts
@@ -8,10 +8,10 @@ import {
   PBXSourcesBuildPhase,
   type AnyBuildPhase,
 } from "./PBXSourcesBuildPhase";
+import { PBXFileReference } from "./PBXFileReference";
 import type { PickRequired, SansIsa } from "./utils/util.types";
 import type { XcodeProject } from "./XcodeProject";
 import type { PBXBuildRule } from "./PBXBuildRule";
-import type { PBXFileReference } from "./PBXFileReference";
 import type { PBXTargetDependency } from "./PBXTargetDependency";
 import type { XCConfigurationList } from "./XCConfigurationList";
 import type { XCSwiftPackageProductDependency } from "./XCSwiftPackageProductDependency";
@@ -89,6 +89,48 @@ export class PBXNativeTarget extends AbstractTarget<PBXNativeTargetModel> {
       this.getBuildPhase(PBXResourcesBuildPhase) ??
       this.createBuildPhase(PBXResourcesBuildPhase)
     );
+  }
+
+  /** Ensures a list of frameworks are linked to the target, given a list like `['SwiftUI.framework', 'WidgetKit.framework']`. Also ensures the file references are added to the Frameworks display folder. */
+  ensureFrameworks(frameworks: string[]) {
+    const frameworksFolder =
+      this.getXcodeProject().rootObject.getFrameworksGroup();
+
+    // TODO: This might need OS-specific checks like https://github.com/CocoaPods/Xcodeproj/blob/ab3dfa504b5a97cae3a653a8924f4616dcaa062e/lib/xcodeproj/project/object/native_target.rb#L322-L328
+    const getFrameworkFileReference = (name: string): PBXFileReference => {
+      const frameworkName = name.endsWith(".framework")
+        ? name
+        : name + ".framework";
+
+      for (const [, entry] of this.getXcodeProject().entries()) {
+        if (
+          PBXFileReference.is(entry) &&
+          entry.props.lastKnownFileType === "wrapper.framework" &&
+          entry.props.sourceTree === "SDKROOT" &&
+          entry.props.name === frameworkName
+        ) {
+          // This should never happen but if it does then we can repair the state by adding the framework file reference to the Frameworks display group.
+          if (
+            !frameworksFolder.props.children.find(
+              (child) => child.uuid === entry.uuid
+            )
+          ) {
+            frameworksFolder.props.children.push(entry);
+          }
+          return entry;
+        }
+      }
+
+      return frameworksFolder.createFile({
+        path: "System/Library/Frameworks/" + frameworkName,
+      });
+    };
+
+    return frameworks.map((framework) => {
+      return this.getFrameworksBuildPhase().ensureFile({
+        fileRef: getFrameworkFileReference(framework),
+      });
+    });
   }
 
   protected getObjectProps(): Partial<{
